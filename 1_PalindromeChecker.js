3d0f2053c9845320a46b6e5c4781121ddd114495
climberomes <126999678+climberomes@users.noreply.github.com>
2023-03-16 17:16:03 -0500

    Update and rename PalindromeChecker.js to 1_PalindromeChecker.js

--- 1_PalindromeChecker.js
+++ 1_PalindromeChecker.js
@@ -11,7 +11,7 @@ We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR 
 We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.
 
 Tests:
-palindrome("eye") should return a boolean.
+Passed:palindrome("eye") should return a boolean.
 Passed:palindrome("eye") should return true.
 Passed:palindrome("_eye") should return true.
 Passed:palindrome("race car") should return true.
